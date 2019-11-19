a18b18fd08bfc934f0de6df67a616f373bd89787
Reko Meriö <K9260@student.jamk.fi>
2019-11-19 10:06:06 +0200

    File cleanup

--- server.js
+++ server.js
@@ -1,140 +1,55 @@
-//  OpenShift sample Node application
-var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
+const express = require("express");
+const morgan = require("morgan");
+const mongodb = require("mongodb");
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+const app = express();
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+app.use(morgan("combined"));
 
-if (mongoURL == null) {
-  var mongoHost, mongoPort, mongoDatabase, mongoPassword, mongoUser;
-  // If using plane old env vars via service discovery
-  if (process.env.DATABASE_SERVICE_NAME) {
-    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
-    mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'];
-    mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'];
-    mongoDatabase = process.env[mongoServiceName + '_DATABASE'];
-    mongoPassword = process.env[mongoServiceName + '_PASSWORD'];
-    mongoUser = process.env[mongoServiceName + '_USER'];
+const PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const IP = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
+let mongoUrl = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
 
-  // If using env vars from secret from service binding  
-  } else if (process.env.database_name) {
-    mongoDatabase = process.env.database_name;
-    mongoPassword = process.env.password;
-    mongoUser = process.env.username;
-    var mongoUriParts = process.env.uri && process.env.uri.split("//");
-    if (mongoUriParts.length == 2) {
-      mongoUriParts = mongoUriParts[1].split(":");
-      if (mongoUriParts && mongoUriParts.length == 2) {
-        mongoHost = mongoUriParts[0];
-        mongoPort = mongoUriParts[1];
-      }
-    }
-  }
+if (!mongoUrl && process.env.DATABASE_SERVICE_NAME) {
+    let mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
+    let mongoHost = process.env[mongoServiceName + "_SERVICE_HOST"];
+    let mongoPort = process.env[mongoServiceName + "_SERVICE_PORT"];
+    let mongoDatabase = process.env[mongoServiceName + "_DATABASE"];
+    let mongoPassword = process.env[mongoServiceName + "_PASSWORD"];
+    let mongoUser = process.env[mongoServiceName + "_USER"];
 
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-  }
+    mongoUrl =
+        "mongodb://" +
+        mongoUser +
+        ":" +
+        mongoPassword +
+        "@" +
+        mongoHost +
+        ":" +
+        mongoPort +
+        "/" +
+        mongoDatabase;
 }
-var db = null,
-    dbDetails = new Object();
 
-var initDb = function(callback) {
-  if (mongoURL == null) return;
+let dbo;
 
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
-
-  mongodb.connect(mongoURL, function(err, conn) {
+mongodb.connect(mongoUrl, (err, db) => {
     if (err) {
-      callback(err);
-      return;
+        console.error(err);
+        return;
     }
-
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
-};
-
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      if (err) {
-        console.log('Error running count. Message:\n'+err);
-      }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
+    dbo = db;
 });
 
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send(":(");
-  }
+app.get("/", (req, res) => {
+    dbo.collection("counts").insert({ ip: req.ip, date: Date.now() });
+    dbo.collection("counts")
+        .find()
+        .toArray((err, result) => {
+            res.send(result);
+        });
 });
 
-app.get('/events', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').find().toArray((err, result) => {
-      res.send(result);
-    });
-  } else {
-    res.send(":(");
-  }
-});
-
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
-
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
-
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
+app.listen(PORT, IP);
 
 module.exports = app;
