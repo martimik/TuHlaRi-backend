be1c3b52787a4a1c3ed6becc58dfe1e1c18c84be
opiskelija <opiskelija@IT-L-R90LR1WM.it.local>
2019-11-25 14:57:12 +0200

    login/logout start

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongodb = require("mongodb");
+const session = require("express-session");
 
 const app = express();
 const { ObjectId } = mongodb;
@@ -9,7 +10,9 @@ app.use(morgan("combined"));
 
 const PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 const IP = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
-let mongoUrl = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
+let mongoUrl = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL || "mongodb://127.0.0.1:27017/development";
+
+console.log(process.env.MONGO_URL);
 
 if (!mongoUrl && process.env.DATABASE_SERVICE_NAME) {
     const mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
@@ -43,8 +46,10 @@ mongodb.connect(mongoUrl, (err, database) => {
     console.log("Connected to MongoDB at: %s", mongoUrl);
 });
 
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000, }}))
+
 app.get("/", (req, res) => {
-    res.send("Hello");
+    res.send("Hello with sessions. :)")
 });
 
 app.get("/events", (req, res) => {
@@ -64,7 +69,52 @@ app.get("/event/:id", (req, res) => {
     );
 });
 
+app.get("/login/:name.:password", (req, res) => {
+    
+    
+    console.log("name: " + req.params.name);
+    console.log("pass: " + req.params.password);
+
+    db.collection("development").findOne({
+        "name": req.params.name,
+        "password": req.params.password
+    }), (err, result) => {
+        console.log("ok");
+        console.log(result);
+        console.log(err);
+        if(result != "null"){
+
+            req.session.id = result.id;
+            req.session.name = result.name;
+            req.session.email = result.email;
+            req.session.userGroup = result.userGroup;
+
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify({ name: result.name, email: result.email, userGroup: result.userGroup}));
+        }
+        else{
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify({ error: "Invalid login creditentials."}));
+        }
+    }
+});
+
+app.get("/logout", (req, res) => {
+    if (req.session) {
+        req.session.destroy(function(err) {
+            if(err) {
+                return next(err);
+            } else {
+                req.session = null;
+                console.log("logout successful");
+                return res.send("Logout");
+            }
+        });
+    }
+});
+
 app.listen(PORT, IP);
 console.log("Server running on http://%s:%s", IP, PORT);
 
 module.exports = app;
+
