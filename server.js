559f9702f76b5d17ad6ca69ddf5a85fd0cf140ee
opiskelija <opiskelija@IT-L-R90LR1WM.it.local>
2019-11-26 16:33:19 +0200

    backend progress

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require("express");
 const morgan = require("morgan");
 const mongodb = require("mongodb");
 const session = require("express-session");
+const cors = require("cors");
 const bodyParser = require("body-parser");
+const { body, validationResult } = require("express-validator/check");
+const sanitizeBody = require("express-validator/filter");
+const sha256 = require("sha256");
 
 const app = express();
 const { ObjectId } = mongodb;
 
 app.use(morgan("combined"));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({ 
@@ -78,16 +83,16 @@ app.get("/event/:id", (req, res) => {
     );
 });
 
-app.post("/login/", (req, res) => {
+app.post("/login", (req, res) => {
 
     console.log("name: " + req.body.name);
     console.log("pass: " + req.body.password);
+
+    let passwordHash = sha256(req.body.password);
     
-    db.collection("development").findOne({ name: req.body.name, password: req.body.password}, function(err, result) {
+    db.collection("development").findOne({ name: req.body.name, password: passwordHash}, function(err, result) {
 
         console.log("ok");
-        console.log(result);
-        console.log(err);
 
         if(result != null){
 
@@ -106,40 +111,79 @@ app.post("/login/", (req, res) => {
     });
 });
 
-app.get("/logout", (req, res, next) => {
+app.post("/logout", (req, res, next) => {
     if (req.session) {
         req.session.destroy(function(err) {
             if(err) {
                 next(err);
             } else {
                 req.session = null;
-                res.write("Logout:");
-                res.write(JSON.stringify({ name: req.session.name, email: req.session.email, userGroup: req.session.userGroup}));
-                res.send();
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify({ name: null, email: null, userGroup: null}));
             }
         });
     }
+    else {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ error: "Session does not exist."}));
+    }
 });
 
-app.post("/newUser", function(req, res, next) {
+app.post("/test", 
+    [ 
+        body('name').isLength({ min: 1 }).isString().escape(),
+        body('email').isLength({ min: 1 }).isString().isEmail().escape(),
+        body('password').isLength({ min: 1 }).isString().escape(),
+        body('userGroup').isLength({ min: 1 }).isString().escape()
+    ], function(req, res, next){
 
-    if(req.session.userGroup != 1){
-        res.end(JSON.stringify({ error: "Need admin privileges."}));
-    } else next();
-    }, 
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        res.send("error");
+    } else {
+        res.send("ok");  
+    }
+}
+)
+
+app.post("/newUser",
+    [
+        body('name').isLength({ min: 1 }).isString().escape(),
+        body('email').isLength({ min: 1 }).isString().isEmail().escape(),
+        body('password').isLength({ min: 1 }).isString().escape(),
+        body('userGroup').isLength({ min: 1 }).isInt().escape()
+    ],/*
     function(req, res, next){
-        if(typeof req.body.name != String || typeof req.body.email != String || typeof req.body.password != String || typeof req.body.userGroup != Int){
-            res.setHeader('Content-Type', 'application/json');
-            res.write(JSON.stringify({ error: "Invalid data :("}));
-            res.end();
+        if(req.session.userGroup != "1"){
+            res.end(JSON.stringify({ error: "Need admin privileges."}));
         } else next();
     },
+*/   function(req, res, next) {
+
+    if(!validationResult(req).isEmpty()){
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ error: "Invalid form data."}));
+    } else next();  
+    },
     function(req, res, next){
+        console.log(req.body.email)
+        db.collection("development").findOne({ email: req.body.email },
+        function(err, result) {
+            console.log(result);
+            if(result != null){
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify({ error: "Email already in use."}));
+            } else next();
+        }
+    )},
+    function(req, res, next){
+
+    let passwordHash = sha256(req.body.password);
 
     db.collection("development").insert({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: passwordHash,
         userGroup: req.body.userGroup
     }, function(err, result) {
         if(result.nInserted == 0 || err){
@@ -149,20 +193,94 @@ app.post("/newUser", function(req, res, next) {
         } else {
             res.setHeader('Content-Type', 'application/json');
             res.write("Adding user succ :D");
-            res.write(JSON.stringify({ name: req.body.name, email: req.body.email, userGroup: req.body.userGroup}));
+            res.write(JSON.stringify({ name: req.body.name, email: req.body.email, password: req.body.password, userGroup: req.body.userGroup}));
             res.send();
         }
     });
 });
 
-var validateUserData = (req, res, next) => {
-    if(typeof req.body.name != String || typeof req.body.email != String || typeof req.body.password != String || typeof req.body.userGroup != Int){
-        res.setHeader('Content-Type', 'application/json');
-        res.write(JSON.stringify({ error: "Invalid data :("}));
-        res.end();
-    };
-};
+app.route("/products")
+    .post(
+        [
+            body('productName').isLength({ min: 1 }).isString().escape(),
+            body('shortDescription').isLength({ min: 1 }).isString().escape(),
+            body('logo').isString().escape().optional(),
+            body('longDescription').isString().escape().optional(),
+            body('technologies').isString().escape().optional(),
+            body('enviromentRequirements').isString().escape().optional(),
+            body('customer').isString().escape().optional(),
+            body('lifeCycle').isString().escape(),
+            body('owner').isString().escape().optional(),
+            body('price').isString().escape().optional()
+        ], function(req, res, next) {
+            console.log("ok?");
+            if(!validationResult(req).isEmpty()){
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify({ error: "Invalid form data."}));
+            } else next();  
+        },/*function(req, res, next) {
+            if(req.session.userGroup != ("1" || "2" || "3")){
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify({ error: "Insufficient priviledges."}));
+            } else next();
+        },*/ function(req, res, next) {
+            console.log("ok");
+            db.collection("development").insert({
+                productName: req.body.productName,
+                shortDescription: req.body.shortDescription,
+                logo: req.body.logo,
+                longDescription: req.body.longDescription,
+                technologies: req.body.technologies,
+                longDescription: req.body.longDescription,
+                enviromentRequirements: req.body.enviromentRequirements,
+                customer: req.body.customer,
+                lifeCycle: req.body.lifeCycle,
+                owner: req.body.owner,
+                price: req.body.price
+            }, function(err, result) {
+                if(result.nInserted == 0 || err){
+                    res.setHeader('Content-Type', 'application/json');
+                    res.send(JSON.stringify({ error: "Insert failed."}));
+                    next(err);
+                } else {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.write("Adding product succ :D");
+                    res.write(JSON.stringify({ name: req.body.name, shortDescription: req.body.shortDescription}));
+                    res.send();
+                }
+            });
+        }
+    )
 
+    .get(function (req, res, next) {
+        db.collection("development").find({$text: {$search: req.body.search}
+        }, function(err, result) {
+            if(result == null) {
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify({ error: "Nothing found."}));
+                next();
+            } else {
+                res.setHeader('Content-Type', 'application/json');
+                    res.write("Searching product succ :D");
+                    console.log(result);
+                    res.write(JSON.stringify({
+                        productName: result.productName,
+                        shortDescription: result.shortDescription,
+                        logo: result.logo,
+                        longDescription: result.longDescription,
+                        technologies: result.technologies,
+                        longDescription: result.longDescription,
+                        enviromentRequirements: result.enviromentRequirements,
+                        customer: result.customer,
+                        lifeCycle: result.lifeCycle,
+                        owner: result.owner,
+                        price: result.price
+                    }));
+                    res.send();
+            }
+        }
+    );
+})
 
 app.use(function(err, req, res, next) {
     console.error(err.stack);
