7f474ec3b40b9d19b69c03984ed45f45723ea548
Back-end PC <back@end.com>
2020-02-10 14:38:12 +0200

    add tests - Elias

--- tests/app_test.js
+++ tests/app_test.js
@@ -1,33 +1,146 @@
-/*
-var server   = require('../server'),
-    chai     = require('chai'),
-    chaiHTTP = require('chai-http'),
-    should   = chai.should();
+var server = require("../server"),
+    chai = require("chai"),
+    chaiHTTP = require("chai-http"),
+    should = chai.should();
 
 chai.use(chaiHTTP);
 
-reqServer = process.env.HTTP_TEST_SERVER || server
+reqServer = process.env.HTTP_TEST_SERVER || server;
 
-describe('Basic routes tests', function() {
-
-    it('GET to / should return 200', function(done){
+describe("Basic routes tests", function() {
+    it("GET to / should return 200", function(done) {
         chai.request(reqServer)
-        .get('/')
-        .end(function(err, res) {
-            res.should.have.status(200);
-            done();
-        })
+            .get("/")
+            .end(function(err, res) {
+                res.should.have.status(200);
+                done();
+            });
+    });
 
-    })
-
-    it('GET to /pagecount should return 200', function(done){
+    it("GET to /session should return 200", function(done) {
         chai.request(reqServer)
-        .get('/pagecount')
-        .end(function(err, res) {
-            res.should.have.status(200);
-            done();
-        })
+            .get("/session")
+            .end(function(err, res) {
+                res.should.have.status(200);
+                done();
+            });
+    });
 
-    })
-})
-*/
+    /* To be done in the future maybe 
+   it("Get users without logging in", function(done) {
+        chai.request(reqServer)
+            .get("/users")
+            .end(function(err, res) {
+                res.should.have.status(401);
+                done();
+            });
+    }); */
+
+    it("Setup session and GET users", function(done) {
+        chai.request(reqServer)
+            .get("/users")
+            .auth("jorma@mail.com", "jorma")
+            .end(function(err, res) {
+                console.log(res.body);
+                res.should.have.status(200);
+                res.body.should.be.a("array");
+                done();
+            });
+    });
+
+    it("Adding a product without logging in", function(done) {
+        chai.request(reqServer)
+            .post("/addProduct")
+            .send({
+                productName: "me",
+                shortDescription: "123",
+                lifecycleStatus: 1
+            })
+            .end(function(err, res) {
+                res.should.have.status(401);
+                done();
+            });
+    });
+
+    it("Editing password without logging in", function(done) {
+        chai.request(reqServer)
+            .post("/editPassword")
+            .send({
+                oldPassword: "123456",
+                password: "12345678"
+            })
+            .end(function(err, res) {
+                res.should.have.status(401);
+                done();
+            });
+    });
+
+    it("Editing a product without logging in", function(done) {
+        chai.request(reqServer)
+            .post("/editProduct")
+            .send({
+                productName: "me",
+                shortDescription: "123",
+                lifecycleStatus: 1
+            })
+            .end(function(err, res) {
+                res.should.have.status(401);
+                done();
+            });
+    });
+
+    it("Restoring a product without logging in", function(done) {
+        chai.request(reqServer)
+            .post("/restoreProduct")
+            .send({
+                _id: "paavo"
+            })
+            .end(function(err, res) {
+                res.should.have.status(401);
+                done();
+            });
+    });
+
+    it("Deleting a user without Admin priviledges", function(done) {
+        chai.request(reqServer)
+            .post("/deleteUser")
+            .send({
+                email: "paavo@paavo.com"
+            })
+            .end(function(err, res) {
+                res.should.have.status(401);
+                done();
+            });
+    });
+
+    it("Creating a new user without Admin priviledges", function(done) {
+        chai.request(reqServer)
+            .post("/newUser")
+            .send({
+                name: "Paavo",
+                email: "paavo@paavo.com",
+                password: "Pave",
+                userGroup: 3
+            })
+            .end(function(err, res) {
+                res.should.have.status(401);
+                done();
+            });
+    });
+
+    it("Editing a user without Admin priviledges", function(done) {
+        chai.request(reqServer)
+            .post("/editUser")
+            .send({
+                reqEmail: "paavo@paavo.com",
+                name: "Paavo",
+                email: "paavo@paavo.com",
+                password: "Pave",
+                userGroup: 3
+            })
+            .end(function(err, res) {
+                res.should.have.status(401);
+                done();
+            });
+    });
+});
--- utils/checkAdminPriviledges.js
+++ utils/checkAdminPriviledges.js
@@ -1,5 +1,10 @@
 module.exports = (req, res, next) => {
     if (req.session.userGroup !== "0") {
-        res.json({ error: "Need admin privileges." });
+        res.setHeader("Content-Type", "application/json");
+        res.status(401);
+        res.send({
+            message: "Unauthorized, need admin privileges",
+            code: "UAE2"
+        });
     } else next();
 };
